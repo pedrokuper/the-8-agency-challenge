7c6dbfff5dd4f8b8b04554ea4f722acd5d8d22e2
Pedro <pkupersmit@gmail.com>
2021-04-29 21:26:21 -0300

    Contact page styles added

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./Contact.module.scss";
 
 //Components
 import Navbar from "../../components/Navbar";
@@ -20,61 +21,72 @@ import { AiOutlineGooglePlus } from "react-icons/ai";
 
 function Contact(props) {
   return (
-    <div>
+    <>
       <Navbar />
-      <div>
-        <div>
-          <h4>Contact us via email, phone or come by into our office!</h4>
-          <div>
-            <FaLinkedin />
-            <FaFacebookSquare />
-            <FaInstagram />
-            <AiOutlineGooglePlus />
+      <div className={style.wrapper}>
+        <div className={style.contentWrapper}>
+          <div className={style.socialContainer}>
+            <h4>Contact us via email, phone or come by into our office!</h4>
+            <div className={style.iconContainer}>
+              <span>
+                <FaLinkedin />
+              </span>
+              <span>
+                <FaFacebookSquare />
+              </span>
+              <span>
+                <FaInstagram />
+              </span>
+              <span>
+                <AiOutlineGooglePlus />
+              </span>
+            </div>
           </div>
+          <div className={style.infoContainer}>
+            <div className={style.contactWrapper}>
+              <span>
+                <BsEnvelope className={`${style.icon} ${style.envelope}`} />
+                <a href="mailto:info@airhitect.com">info@airhitect.com</a>
+              </span>
+              <span>
+                <FiPhoneCall className={style.icon} />
+                +12 34 567 890 <br /> +09 87 654 321
+              </span>
+              <span>
+                <VscHome className={style.icon} />
+                1012 Budapest <br />
+                Márvány utca 16
+              </span>
+            </div>
+            <div className={style.contactInfo}>
+              <h5>We're hiring</h5>
+              <p>
+                We are looking for new talents to our team! If you think you are
+                the right person for AIRhitect send us an email with your CV and
+                portfolio. Don’t forget to write a little about yourself!
+              </p>
+              <a href="mailto:job@airhitect.com">job@airhitect.com</a>
+            </div>
+          </div>
+          <form className={style.container}>
+            <Input name="name" type="text" placeholder="Name" />
+            <Input name="email" type="email" placeholder="E-mail" />
+            <Input name="subject" type="text" placeholder="Subject" />
+            <textarea
+              className={style.textarea}
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+          </form>
         </div>
         <div>
-          <div>
-            <span>
-              <BsEnvelope />
-              info@airhitect.com
-            </span>
-            <span>
-              <FiPhoneCall />
-              +12 34 567 890 <br /> +09 87 654 321
-            </span>
-            <span>
-              <VscHome />
-              1012 Budapest <br />
-              Márvány utca 16
-            </span>
-          </div>
-          <div>
-            <h5>We're hiring</h5>
-            <p>
-              We are looking for new talents to our team! If you think you are
-              the right person for AIRhitect send us an email with your CV and
-              portfolio. Don’t forget to write a little about yourself!
-              job@airhitect.com
-            </p>
-          </div>
-        </div>
-        <div>
-          <Input name="name" type="text" placeholder="Name" />
-          <Input name="email" type="email" placeholder="E-mail" />
-          <Input name="subject" type="text" placeholder="Subject" />
-          <textarea
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            placeholder="Message"
-          ></textarea>
+          <img src={map} />
         </div>
       </div>
-      <div>
-        <img src={map} />
-      </div>
-    </div>
+    </>
   );
 }
 
